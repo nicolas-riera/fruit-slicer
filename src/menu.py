a424c3bb00a3864d68f6cccaaf94c1b8f43289dd
nicolas-riera <nicolas.riera@laplateforme.io>
2026-01-27 11:13:36 +0100

    fix: separated menu rendering and menu logic

--- src/menu.py
+++ src/menu.py
@@ -8,6 +8,39 @@ from src.game import game
 
 # Functions
 
+def menu_rendering(screen, my_fonts, play_hover, option_hover):
+
+    background_rect = BACKGROUND_IMG.get_rect(topleft=(0, 0))
+    background_scaled = pygame.transform.smoothscale(BACKGROUND_IMG, (BACKGROUND_IMG.get_size()[0]*0.84, BACKGROUND_IMG.get_size()[1]*0.84))
+    screen.blit(background_scaled, background_rect)
+
+    logo_title_rect = LOGO_TITLE.get_rect(center=(1130, 550))
+    logo_title_scaled = pygame.transform.smoothscale(LOGO_TITLE, (LOGO_TITLE.get_size()[0]*0.4, LOGO_TITLE.get_size()[1]*0.4))
+    screen.blit(logo_title_scaled, logo_title_rect) 
+
+    wood_button_scaled = pygame.transform.smoothscale(WOOD_BUTTON, (WOOD_BUTTON.get_size()[0]*0.27, WOOD_BUTTON.get_size()[1]*0.27))
+    wood_button_hover_scaled = pygame.transform.smoothscale(WOOD_BUTTON_HOVER, (WOOD_BUTTON_HOVER.get_size()[0]*0.27, WOOD_BUTTON_HOVER.get_size()[1]*0.27))
+    wood_button_rect_play = wood_button_scaled.get_rect(center=(640, 440))
+    wood_button_rect_option = wood_button_scaled.get_rect(center=(640, 590))
+
+    if play_hover:
+        screen.blit(wood_button_hover_scaled, wood_button_rect_play) 
+    else:
+        screen.blit(wood_button_scaled, wood_button_rect_play) 
+    play_button = pygame.Rect((464, 390, 356, 100))
+    play_button_text = my_fonts[2].render("Play", True, (254, 250, 181))
+    screen.blit(play_button_text, (597, 416))
+
+    if option_hover:
+        screen.blit(wood_button_hover_scaled, wood_button_rect_option) 
+    else:
+        screen.blit(wood_button_scaled, wood_button_rect_option) 
+    option_button = pygame.Rect((464, 540, 356, 100))
+    option_button_text = my_fonts[2].render("Options", True, (254, 250, 181))
+    screen.blit(option_button_text, (564, 567))  
+
+    return play_button, option_button
+
 def menu(screen, clock, my_fonts):
 
     screen_scale = 1
@@ -16,41 +49,10 @@ def menu(screen, clock, my_fonts):
     option_hover = False
 
     while True:
-        
-        # pygame events
 
         events, mouseclicked, escpressed = pygame_events()
 
-        # Rendering 
-
-        background_rect = BACKGROUND_IMG.get_rect(topleft=(0, 0))
-        background_scaled = pygame.transform.smoothscale(BACKGROUND_IMG, (BACKGROUND_IMG.get_size()[0]*0.84, BACKGROUND_IMG.get_size()[1]*0.84))
-        screen.blit(background_scaled, background_rect)
-
-        logo_title_rect = LOGO_TITLE.get_rect(center=(1130, 550))
-        logo_title_scaled = pygame.transform.smoothscale(LOGO_TITLE, (LOGO_TITLE.get_size()[0]*0.4, LOGO_TITLE.get_size()[1]*0.4))
-        screen.blit(logo_title_scaled, logo_title_rect) 
-
-        wood_button_scaled = pygame.transform.smoothscale(WOOD_BUTTON, (WOOD_BUTTON.get_size()[0]*0.27, WOOD_BUTTON.get_size()[1]*0.27))
-        wood_button_hover_scaled = pygame.transform.smoothscale(WOOD_BUTTON_HOVER, (WOOD_BUTTON_HOVER.get_size()[0]*0.27, WOOD_BUTTON_HOVER.get_size()[1]*0.27))
-        wood_button_rect_play = wood_button_scaled.get_rect(center=(640, 440))
-        wood_button_rect_option = wood_button_scaled.get_rect(center=(640, 590))
-
-        if play_hover:
-            screen.blit(wood_button_hover_scaled, wood_button_rect_play) 
-        else:
-            screen.blit(wood_button_scaled, wood_button_rect_play) 
-        play_button = pygame.Rect((464, 390, 356, 100))
-        play_button_text = my_fonts[2].render("Play", True, (254, 250, 181))
-        screen.blit(play_button_text, (597, 416))
-
-        if option_hover:
-            screen.blit(wood_button_hover_scaled, wood_button_rect_option) 
-        else:
-            screen.blit(wood_button_scaled, wood_button_rect_option) 
-        option_button = pygame.Rect((464, 540, 356, 100))
-        option_button_text = my_fonts[2].render("Options", True, (254, 250, 181))
-        screen.blit(option_button_text, (564, 567))  
+        play_button, option_button = menu_rendering(screen, my_fonts, play_hover, option_hover)
 
         # Logic
 

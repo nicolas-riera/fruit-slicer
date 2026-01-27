928e0f6b0a364c42b1a8513afbc22d06de80066e
André <gwandrewgd@gmail.com>
2026-01-27 16:35:12 +0100

    fix(game): made fruits dictionary better so elements are also a dict.

--- src/game.py
+++ src/game.py
@@ -11,10 +11,11 @@ from src.assets_loading import fruits_src
 # Functions
 
 def create_fruit(fruits):
-    fruit_name = random.choice(list(fruits_src.items()))
-    fruits[fruit_name] = [
-        (random.randint(-400, 1500), random.randint(-100, -10)), random.choice(string.ascii_letters)
-    ]
+    fruits[len(fruits)] = {
+        "fruit": random.choice(list(fruits_src.items())),
+        "pos": (random.randint(-400, 1500), random.randint(-100, -10)),
+        "letters": random.choice(string.ascii_letters).lower()
+    }
 
     return fruits
 
@@ -22,12 +23,9 @@ def create_fruit(fruits):
 def game(screen, clock):
 
     running = True
-    # fruits_pos = {
-    #     "apple0": [(300, 200), "k"]
-    # }
 
     fruits = {}
-
+    counter = 0 #temporary just for now for me to test new fruits to not be popping all the time 
     while running:
         events, mouseclicked, escpressed = pygame_events()
 
@@ -35,11 +33,13 @@ def game(screen, clock):
             running = False
 
         ui_render(screen)
-        fruits = create_fruit(fruits)
+        if counter % 240 == 0 or counter == 0:
+            fruits = create_fruit(fruits)
+            print(fruits)
 
         # Logic
 
-
+        counter += 1
         pygame.display.flip()
         clock.tick(60)
 

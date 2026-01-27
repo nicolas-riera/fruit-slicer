836f9c361d04c4865736c9b2c11b60b8d5ab0c87
nicolas-riera <nicolas.riera@laplateforme.io>
2026-01-27 13:41:50 +0100

    fix: improved button hover logic and structure

--- src/menu.py
+++ src/menu.py
@@ -5,10 +5,11 @@ import pygame
 from src.assets_loading import LOGO_TITLE, BACKGROUND_IMG, WOOD_BUTTON, WOOD_BUTTON_HOVER
 from src.pygame_events import *
 from src.game import game
+from src.popup import *
 
 # Functions
 
-def menu_rendering(screen, my_fonts, play_hover, option_hover):
+def menu_rendering(screen, my_fonts):
 
     background_rect = BACKGROUND_IMG.get_rect(topleft=(0, 0))
     background_scaled = pygame.transform.smoothscale(BACKGROUND_IMG, (BACKGROUND_IMG.get_size()[0]*0.84, BACKGROUND_IMG.get_size()[1]*0.84))
@@ -23,62 +24,49 @@ def menu_rendering(screen, my_fonts, play_hover, option_hover):
     wood_button_rect_play = wood_button_scaled.get_rect(center=(640, 440))
     wood_button_rect_option = wood_button_scaled.get_rect(center=(640, 590))
 
-    if play_hover:
+    play_button = pygame.Rect((464, 390, 356, 100))
+    if play_button.collidepoint(pygame.mouse.get_pos()):
         screen.blit(wood_button_hover_scaled, wood_button_rect_play) 
     else:
         screen.blit(wood_button_scaled, wood_button_rect_play) 
-    play_button = pygame.Rect((464, 390, 356, 100))
     play_button_text = my_fonts[2].render("Play", True, (254, 250, 181))
     screen.blit(play_button_text, (597, 416))
 
-    if option_hover:
+    option_button = pygame.Rect((464, 540, 356, 100))
+    if option_button.collidepoint(pygame.mouse.get_pos()):
         screen.blit(wood_button_hover_scaled, wood_button_rect_option) 
     else:
         screen.blit(wood_button_scaled, wood_button_rect_option) 
-    option_button = pygame.Rect((464, 540, 356, 100))
     option_button_text = my_fonts[2].render("Options", True, (254, 250, 181))
-    screen.blit(option_button_text, (564, 567))  
+    screen.blit(option_button_text, (564, 567)) 
 
-    return play_button, option_button
+    return play_button, option_button 
 
 def menu(screen, clock, my_fonts):
 
     screen_scale = 1
 
-    play_hover = False
-    option_hover = False
-
     while True:
 
         events, mouseclicked, escpressed = pygame_events()
 
-        play_button, option_button = menu_rendering(screen, my_fonts, play_hover, option_hover)
+        play_button, option_button = menu_rendering(screen, my_fonts)
 
         if escpressed:
             pygame.quit()
             raise SystemExit
 
         elif play_button.collidepoint(pygame.mouse.get_pos()) or option_button.collidepoint(pygame.mouse.get_pos()):
-            if play_button.collidepoint(pygame.mouse.get_pos()):
-                if mouseclicked:
-                    pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)  
+            if mouseclicked:
+                pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW) 
+                if play_button.collidepoint(pygame.mouse.get_pos()):
                     game(screen, clock, events, mouseclicked, escpressed)
                 else:
-                    play_hover = True
-                    option_hover = False
-                    pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
-
+                    #tbd option
+                    pass
             else:
-                if mouseclicked:
-                    pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)  
-                    # tbd options
-                else:
-                    play_hover = False
-                    option_hover = True
-                    pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+                pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
         else:
-            play_hover = False
-            option_hover = False
             pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
 
         pygame.display.flip()  

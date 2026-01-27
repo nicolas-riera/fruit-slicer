dd6b9b9c229143848ef98e5a264e1215e09a9c28
nicolas-riera <nicolas.riera@laplateforme.io>
2026-01-27 10:56:16 +0100

    feat: added button hover in menu

--- src/assets_loading.py
+++ src/assets_loading.py
@@ -9,4 +9,9 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 LOGO_TITLE = pygame.image.load(os.path.join(BASE_DIR, "..", "assets", "img", "logo_title.png"))
 BACKGROUND_IMG = pygame.image.load(os.path.join(BASE_DIR, "..", "assets", "img", "background.png"))
+
 WOOD_BUTTON = pygame.image.load(os.path.join(BASE_DIR, "..", "assets", "img", "wood_button.png"))
+WOOD_BUTTON_HOVER = WOOD_BUTTON.copy()
+filtre = pygame.Surface(WOOD_BUTTON.get_size(), pygame.SRCALPHA)
+filtre.fill((25, 10, 0, 0))
+WOOD_BUTTON_HOVER.blit(filtre, (0, 0), special_flags=pygame.BLEND_RGBA_ADD)
--- src/menu.py
+++ src/menu.py
@@ -3,7 +3,7 @@
 import os
 import pygame
 
-from src.assets_loading import LOGO_TITLE, BACKGROUND_IMG, WOOD_BUTTON
+from src.assets_loading import LOGO_TITLE, BACKGROUND_IMG, WOOD_BUTTON, WOOD_BUTTON_HOVER
 from src.pygame_events import *
 from src.game import game
 
@@ -12,6 +12,9 @@ from src.game import game
 def menu(screen, clock, my_fonts):
 
     screen_scale = 1
+
+    play_hover = False
+    option_hover = False
 
     while True:
         
@@ -30,15 +33,22 @@ def menu(screen, clock, my_fonts):
         screen.blit(logo_title_scaled, logo_title_rect) 
 
         wood_button_scaled = pygame.transform.smoothscale(WOOD_BUTTON, (WOOD_BUTTON.get_size()[0]*0.27, WOOD_BUTTON.get_size()[1]*0.27))
+        wood_button_hover_scaled = pygame.transform.smoothscale(WOOD_BUTTON_HOVER, (WOOD_BUTTON_HOVER.get_size()[0]*0.27, WOOD_BUTTON_HOVER.get_size()[1]*0.27))
         wood_button_rect_play = wood_button_scaled.get_rect(center=(640, 440))
         wood_button_rect_option = wood_button_scaled.get_rect(center=(640, 590))
 
-        screen.blit(wood_button_scaled, wood_button_rect_play) 
+        if play_hover:
+            screen.blit(wood_button_hover_scaled, wood_button_rect_play) 
+        else:
+            screen.blit(wood_button_scaled, wood_button_rect_play) 
         play_button = pygame.Rect((464, 390, 356, 100))
         play_button_text = my_fonts[2].render("Play", True, (254, 250, 181))
         screen.blit(play_button_text, (597, 416))
 
-        screen.blit(wood_button_scaled, wood_button_rect_option) 
+        if option_hover:
+            screen.blit(wood_button_hover_scaled, wood_button_rect_option) 
+        else:
+            screen.blit(wood_button_scaled, wood_button_rect_option) 
         option_button = pygame.Rect((464, 540, 356, 100))
         option_button_text = my_fonts[2].render("Options", True, (254, 250, 181))
         screen.blit(option_button_text, (564, 567))  
@@ -50,18 +60,26 @@ def menu(screen, clock, my_fonts):
             raise SystemExit
 
         elif play_button.collidepoint(pygame.mouse.get_pos()) or option_button.collidepoint(pygame.mouse.get_pos()):
-            if mouseclicked:
-                pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
-                if play_button.collidepoint(pygame.mouse.get_pos()):
+            if play_button.collidepoint(pygame.mouse.get_pos()):
+                if mouseclicked:
+                    pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)  
                     game(screen, clock, events, mouseclicked, escpressed)
-
                 else:
-                    # tbd options
-                    pass
-            else:
-                pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+                    play_hover = True
+                    option_hover = False
+                    pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
 
+            else:
+                if mouseclicked:
+                    pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)  
+                    # tbd options
+                else:
+                    play_hover = False
+                    option_hover = True
+                    pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
         else:
+            play_hover = False
+            option_hover = False
             pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
 
         pygame.display.flip()  

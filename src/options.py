0a4e14144eb1ae99c05ba1263436dbee616a581d
nicolas-riera <nicolas.riera@laplateforme.io>
2026-01-28 15:26:22 +0100

    feat: added fully working clear best score button

--- src/options.py
+++ src/options.py
@@ -1,13 +1,15 @@
 # Libraries
 
 import pygame
+import time
 
 from src.assets_loading import BACKGROUND_RECT, BACKGROUND_SCALED, WOOD_BUTTON, WOOD_BUTTON_HOVER
 from src.pygame_events import pygame_events
+from src.best_score import write_best_score
 
 # Functions
 
-def option_rendering(screen, my_fonts):
+def option_rendering(screen, my_fonts, clear_data_ok_timer):
     
     screen.blit(BACKGROUND_SCALED, BACKGROUND_RECT)
 
@@ -35,8 +37,13 @@ def option_rendering(screen, my_fonts):
         screen.blit(wood_button_hover_scaled, wood_button_rect_clear_best_score) 
     else:
         screen.blit(wood_button_scaled, wood_button_rect_clear_best_score) 
-    clear_best_score_button_text = my_fonts[2].render("Clear Data", True, (254, 250, 181))
-    screen.blit(clear_best_score_button_text, (537, 416))
+
+    if 0 <= time.monotonic() - clear_data_ok_timer <= 2:
+        clear_best_score_button_text = my_fonts[2].render("OK", True, (254, 250, 181))
+        screen.blit(clear_best_score_button_text, (615, 416))
+    else:
+        clear_best_score_button_text = my_fonts[2].render("Clear Data", True, (254, 250, 181))
+        screen.blit(clear_best_score_button_text, (537, 416))
 
     back_button = pygame.Rect((464, 540, 356, 100))
     if back_button.collidepoint(pygame.mouse.get_pos()):
@@ -50,11 +57,13 @@ def option_rendering(screen, my_fonts):
 
 def options(screen, clock, my_fonts, screen_scale):
 
+    clear_data_ok_timer = 0
+
     while True:
 
         events, mouseclicked, escpressed = pygame_events()
 
-        change_scale_button, clear_best_score_button, back_button = option_rendering(screen, my_fonts)
+        change_scale_button, clear_best_score_button, back_button = option_rendering(screen, my_fonts, clear_data_ok_timer)
 
         if escpressed:
             break
@@ -66,7 +75,8 @@ def options(screen, clock, my_fonts, screen_scale):
                     # tbd scale
                     pass
                 elif clear_best_score_button.collidepoint(pygame.mouse.get_pos()):
-                    #tbd clear best score
+                    write_best_score(0)
+                    clear_data_ok_timer = time.monotonic()
                     pass
                 else:
                     break

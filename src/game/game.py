f8bf4851c67fa58bb03927a9922150f42faffc2e
nicolas-riera <nicolas.riera@laplateforme.io>
2026-01-29 15:53:45 +0100

    fix: reduced the max number of fruits at the same time

--- src/game/game.py
+++ src/game/game.py
@@ -106,7 +106,7 @@ def game(screen, clock, my_fonts):
                 if time.time() - time_since_last_fruit >= time_to_spawn:
                     if time_to_spawn == 1 and fruit_rate == 1:
                         time_to_spawn = 4
-                    fruit_count = random.randint(1, min(fruit_rate, 10))
+                    fruit_count = random.randint(1, min(fruit_rate, 7))
                     for i in range(fruit_count):
                         print(i)
                         fruits = create_fruit(fruits)
